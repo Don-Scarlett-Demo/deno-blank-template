c754b9e0c9c02c2dda0916770c77bc4fca40708a
Demo-Jennifer-Hynes <dscarlett@salesforce.com>
2025-07-11 09:43:53 -0400

    Update manifest.ts

--- manifest.ts
+++ manifest.ts
@@ -3,6 +3,7 @@ import { Manifest } from "deno-slack-sdk/mod.ts";
 /**
  * The app manifest contains the app's configuration. This
  * file defines attributes like app name and description.
+ * Note - Tracking additiotnal issues with Locked Records
  * https://api.slack.com/automation/manifest
  */
 export default Manifest({
